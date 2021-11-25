06ace0ed5225c56e0d4769e1de3b1d5bf9ce846c
jzx <945627077@qq.com>
2021-11-25 16:48:14 +0800

    first commit

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    GameObject sphere;
+  
+    void Start()
+    {
+       /** Button b = gameObject.GetComponent<Button>();
+        sphere = GameObject.Find("Sphere");
+       
+        b.onClick.AddListener(delegate () {
+            GameObject s = Instantiate(sphere) as GameObject;
+
+          
+               var p= s.transform.position;
+                p.y = 3;
+                p.z = 0.1F;
+                s.transform.position = p;
+        });
+        **/
+       
+    }
+
+    private void OnMouseDown()
+    {
+       
+    }
+}
---
+++ Assets/Scenes/SphereB.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.transform.position.y);
+        if (this.transform.position.y < -1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

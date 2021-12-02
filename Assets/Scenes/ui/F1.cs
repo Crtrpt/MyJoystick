e00e6a16fb765f1ef3f4a947b2f39ce8ceb3d53f
jzx <945627077@qq.com>
2021-12-02 16:08:08 +0800

    学习unity动画控制器

---
+++ Assets/Scenes/ui/F1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class F1 : MonoBehaviour
+{
+    public Button f1;
+
+    public Button f2;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.f1.onClick.AddListener(Run);
+        this.f2.onClick.AddListener(Attach);
+    }
+
+    void Attach()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+
+        animator.SetBool("attack", !animator.GetBool("attack"));
+        Debug.Log("Attach" + animator.GetBool("attack"));
+    }
+
+    void Run()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+    
+        animator.SetBool("run", !animator.GetBool("run"));
+        Debug.Log("Click"+ animator.GetBool("run"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
